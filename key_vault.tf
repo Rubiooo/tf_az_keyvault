67757a0af06ee4ebf05c62b43ca87358353b1e83
Jiming Chen <jiming.chen@capco.com>
2021-06-16 23:45:57 -0400

    unseal keyvault policy

--- key_vault.tf
+++ key_vault.tf
@@ -8,3 +8,14 @@ resource "azurerm_key_vault" "init_keyvault" {
   enabled_for_disk_encryption     = false
   enabled_for_template_deployment = false
 }
+
+resource "azurerm_key_vault" "unseal_keyvault" {
+  name                            = "unseal${random_string.keyvault_name.result}"
+  resource_group_name             = var.keyvault_rg
+  location                        = var.location
+  tenant_id                       = data.azurerm_client_config.current.tenant_id
+  sku_name                        = "standard"
+  enabled_for_deployment          = true
+  enabled_for_disk_encryption     = false
+  enabled_for_template_deployment = false
+}
